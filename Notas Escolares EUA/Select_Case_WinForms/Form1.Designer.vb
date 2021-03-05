40af5117dbcaabc841d88aa9a1889ebb01c0aa68
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2021-03-05 13:10:31 +0000

    Add files via upload

---
+++ Notas Escolares EUA/Select_Case_WinForms/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtletra = New System.Windows.Forms.TextBox()
+        Me.txtnota = New System.Windows.Forms.TextBox()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtmessage = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Monotype Corsiva", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Crimson
+        Me.Label1.Location = New System.Drawing.Point(4, 15)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(558, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "VB.NET : SELECT CASE STATEMENT EXAMPLE"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Ravie", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Crimson
+        Me.Label2.Location = New System.Drawing.Point(100, 48)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(330, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Grading System in the United States"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Crimson
+        Me.Label3.Location = New System.Drawing.Point(127, 99)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Pontuação (0-100)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Crimson
+        Me.Label4.Location = New System.Drawing.Point(19, 195)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(98, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Nota em letra"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Crimson
+        Me.Label5.Location = New System.Drawing.Point(19, 234)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(70, 15)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Nota(0.0)"
+        '
+        'txtletra
+        '
+        Me.txtletra.BackColor = System.Drawing.Color.Black
+        Me.txtletra.ForeColor = System.Drawing.SystemColors.Info
+        Me.txtletra.Location = New System.Drawing.Point(125, 192)
+        Me.txtletra.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtletra.Name = "txtletra"
+        Me.txtletra.ReadOnly = True
+        Me.txtletra.Size = New System.Drawing.Size(132, 21)
+        Me.txtletra.TabIndex = 5
+        '
+        'txtnota
+        '
+        Me.txtnota.BackColor = System.Drawing.Color.Black
+        Me.txtnota.ForeColor = System.Drawing.SystemColors.Info
+        Me.txtnota.Location = New System.Drawing.Point(125, 228)
+        Me.txtnota.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtnota.Name = "txtnota"
+        Me.txtnota.ReadOnly = True
+        Me.txtnota.Size = New System.Drawing.Size(132, 21)
+        Me.txtnota.TabIndex = 6
+        '
+        'txtScore
+        '
+        Me.txtScore.BackColor = System.Drawing.Color.Black
+        Me.txtScore.ForeColor = System.Drawing.SystemColors.Info
+        Me.txtScore.Location = New System.Drawing.Point(265, 96)
+        Me.txtScore.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(132, 21)
+        Me.txtScore.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.ForeColor = System.Drawing.SystemColors.Info
+        Me.Button1.Location = New System.Drawing.Point(404, 96)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Resultados"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Crimson
+        Me.Label6.Location = New System.Drawing.Point(240, 140)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(89, 16)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Resultados"
+        '
+        'txtmessage
+        '
+        Me.txtmessage.BackColor = System.Drawing.Color.Black
+        Me.txtmessage.ForeColor = System.Drawing.SystemColors.Info
+        Me.txtmessage.Location = New System.Drawing.Point(22, 262)
+        Me.txtmessage.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtmessage.Name = "txtmessage"
+        Me.txtmessage.ReadOnly = True
+        Me.txtmessage.Size = New System.Drawing.Size(375, 21)
+        Me.txtmessage.TabIndex = 11
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(410, 154)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(147, 129)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(570, 294)
+        Me.Controls.Add(Me.txtmessage)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.txtnota)
+        Me.Controls.Add(Me.txtletra)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("MS Reference Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "VB.NET : SELECT CASE STATEMENT EXAMPLE by Code A Minute (iBasskung Tutorial)"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtletra As TextBox
+    Friend WithEvents txtnota As TextBox
+    Friend WithEvents txtScore As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtmessage As TextBox
+End Class
---
+++ Notas Escolares EUA/Select_Case_WinForms/Form1.vb
@@ -0,0 +1,123 @@
+﻿'Link do Video -> https://www.youtube.com/watch?v=gNWQlKMHEOI
+
+'Ativar o que está à frente de Option
+Option Explicit On
+Option Strict On
+
+Public Class Form1
+
+    Private Const myPage1 As String = "1. Code A Minute"
+    Private Const myPage2 As String = "2. iBasskung Tutorial"
+
+    'Button Resultados
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim stringScore As String = txtScore.Text.Trim()
+        Dim stringLetra As String = ""
+        Dim stringNota As String = String.Empty
+        Dim stringMessage As String = ""
+        Dim intScore As Integer
+        'Caminho da pasta onde estão as imgs
+        Dim stringImg As String = "C:\Users\nogue\Desktop\Work\VB\Notas Escolares EUA\Select_Case_WinForms\Fotos_Letras\"
+
+        'Se o utilizador introduzir números
+        If Integer.TryParse(stringScore, intScore) Then
+            If (intScore >= 0) And (intScore <= 100) Then
+                Select Case intScore
+                    'A
+                    Case 90 To 100
+                        stringImg &= "A.png"
+                        stringLetra = "A"
+                        stringNota = "4.0"
+                        stringMessage = "Parabéns!!"
+                    'B
+                    Case 80 To 89
+                        stringImg &= "B.png"
+                        stringLetra = "B"
+                        stringNota = "3.0"
+                        stringMessage = "Boa, tiveste uma boa nota"
+                    'C
+                    Case 74 To 79
+                        stringImg &= "C.png"
+                        stringLetra = "C"
+                        stringNota = "2.0"
+                        stringMessage = "Foi por pouco, mas passas te"
+                    'D
+                    Case 71 To 73
+                        stringImg &= "D.png"
+                        stringLetra = "D"
+                        stringNota = "1.0"
+                        stringMessage = "Não correu muito bem. Para a próxima estuda mais"
+                    Case Else 'E
+                        stringImg &= "F.png"
+                        stringLetra = "F"
+                        stringNota = "0.0"
+                        stringMessage = "Foste um desastre. Para a próxima vê se estudas"
+
+                End Select
+
+                'Exibir a img na PictureBox
+                Dim imgIcon As Image = Image.FromFile(stringImg)
+                PictureBox1.Image = imgIcon
+
+                'Exibir os resulatdos nas Texboxs
+                txtletra.Text = stringLetra
+                txtnota.Text = stringNota
+                txtmessage.Text = stringMessage
+
+            Else
+                Dim caption As String = "VB.NET: Validating User INPUT"
+                Dim button As MessageBoxButtons = MessageBoxButtons.OK
+                Dim icon As MessageBoxIcon = MessageBoxIcon.Information
+
+                'Mensagem
+                MessageBox.Show(Me, "Por favor apenas introduza um número ente 0 e 100" &
+                                Environment.NewLine & vbNewLine &
+                                "Faceboo pages: " & vbNewLine & myPage1 & Environment.NewLine &
+                                myPage2, caption, button, icon)
+
+                'Chamar a função Clear
+                ClearControlValues()
+
+                Return
+            End If
+        Else
+            'Se o utilizar introduzir algo que não seja números
+            Dim caption As String = "VB.NET: Validating User INPUT"
+            Dim button As MessageBoxButtons = MessageBoxButtons.OK
+            Dim icon As MessageBoxIcon = MessageBoxIcon.Exclamation
+
+            'Mensagem
+            MessageBox.Show(Me, "Por favor apenas introduza números" &
+                            Environment.NewLine & vbNewLine &
+                            "Faceboo pages: " & vbNewLine & myPage1 & Environment.NewLine &
+                            myPage2, caption, button, icon)
+
+            'Chamar a função Clear
+            ClearControlValues()
+        End If
+    End Sub
+
+    'Cabeçalho, onde fica o nome da página
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Chamar o Method Clear
+        ClearControlValues()
+    End Sub
+
+    'Create Method Clear
+    Private Sub ClearControlValues()
+        Try
+            'Limpar a img
+            PictureBox1.Image = Nothing
+            'Limpar os valores das textboxs
+            txtScore.Clear()
+            txtletra.Text = ""
+            txtnota.Text = String.Empty
+            txtmessage.Text = ""
+
+            txtScore.Select()
+
+        Catch ex As Exception
+            MessageBox.Show("Erro")
+        End Try
+    End Sub
+End Class
