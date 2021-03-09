a54faa3e16ee744adbb2e94608746d9ce7a6bb3a
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2021-03-09 10:59:58 +0000

    Add files via upload

---
+++ Gráfico Circular 3D/Gráfico Circular/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea3 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Chart1
+        '
+        ChartArea3.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea3)
+        Me.Chart1.Location = New System.Drawing.Point(39, 12)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(455, 285)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(228, 317)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Gerar Gráfico"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(525, 352)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Chart1)
+        Me.Name = "Form1"
+        Me.Text = "Gráfico Circular 3D"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Gráfico Circular 3D/Gráfico Circular/Form1.vb
@@ -0,0 +1,53 @@
+﻿'Link do Video -> https://www.youtube.com/watch?v=GoYVw6swo68
+
+Public Class Form1
+    'Button Gerar Gráfico
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        GerarGrafico()
+    End Sub
+
+    Private Sub GerarGrafico()
+        'Nomes dos produtos
+        Dim produtos() As String = {"Fruta", "Doces", "Salgados", "Vegetais", "Bebidas"}
+        'Quantidade dos produtos
+        Dim valores() As Double = {36798, 56587, 67842, 84987, 43254}
+
+        'Inserir Titulo Principal
+        Dim title = New DataVisualization.Charting.Title()
+        title.Font = New Font("Elephsnt", 22, FontStyle.Bold)
+        title.ForeColor = Color.BlueViolet
+        title.Text = "Vendas de Fevereiro de 2021"
+        Chart1.Titles.Add(title)
+
+        'Inserir Sub Titulo
+        Dim title2 = New DataVisualization.Charting.Title()
+        title2.Font = New Font("Arial", 16, FontStyle.Bold Or FontStyle.Italic)
+        title2.ForeColor = Color.Red
+        title2.Text = "Vendas em Euros"
+        Chart1.Titles.Add(title2)
+
+        'Inserir legenda
+        Dim legend = New DataVisualization.Charting.Legend()
+        Chart1.Legends.Add(legend)
+        Chart1.Legends(0).Title = "Produtos"
+
+        'Vendas reais
+        Chart1.Series.Add("Vendas")
+        With Chart1
+            .Series(0).ChartType = DataVisualization.Charting.SeriesChartType.Pie
+
+            .ChartAreas(0).Area3DStyle.Enable3D = True
+            'Inclinação do gráfico
+            .ChartAreas(0).Area3DStyle.Inclination = 60
+            .ChartAreas(0).Area3DStyle.Rotation = 60
+
+            .Palette = DataVisualization.Charting.ChartColorPalette.SemiTransparent
+
+            'Mostrar os valores sobre as fatias do gráfico circular
+            .Series(0).IsValueShownAsLabel = True
+            'Valores a serem colcoados no gráfico
+            .Series(0).Points.DataBindXY(produtos, valores)
+        End With
+    End Sub
+
+End Class
