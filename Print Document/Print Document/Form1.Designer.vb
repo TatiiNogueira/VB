4e094123e64db06e02c79976e2360a1317b585fb
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2021-03-17 16:41:13 +0000

    Add files via upload

---
+++ Print Document/Print Document/Form1.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewControl1 = New System.Windows.Forms.PrintPreviewControl()
+        Me.SuspendLayout()
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintPreviewControl1
+        '
+        Me.PrintPreviewControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PrintPreviewControl1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.PrintPreviewControl1.ForeColor = System.Drawing.Color.Black
+        Me.PrintPreviewControl1.Location = New System.Drawing.Point(12, 12)
+        Me.PrintPreviewControl1.Name = "PrintPreviewControl1"
+        Me.PrintPreviewControl1.Size = New System.Drawing.Size(267, 265)
+        Me.PrintPreviewControl1.TabIndex = 0
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(296, 296)
+        Me.Controls.Add(Me.PrintPreviewControl1)
+        Me.Name = "form1"
+        Me.Text = "Document"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewControl1 As PrintPreviewControl
+End Class
---
+++ Print Document/Print Document/Form1.vb
@@ -0,0 +1,22 @@
+﻿'Link do Video -> https://www.youtube.com/watch?v=95ZE9gmaYEU
+
+'NOTA: O programa mostra a pré visualização do ficheiro que criamos
+
+Public Class form1
+
+    'Element PrintDocument1
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        'Indicação do tipo de letra e do tamanho para o título e para o texto
+        Dim ReportFont As Font = New Drawing.Font("Time New Roman", 20)
+        Dim ReportBodyFont As Font = New Drawing.Font("Time New Roman", 15)
+        'Indicação do titulo, da cor e da localização
+        'Titulo
+        e.Graphics.DrawString("My Title", ReportFont, Brushes.Chocolate, 150, 100)
+        'Texto
+        e.Graphics.DrawString("O meu texto experimental", ReportBodyFont, Brushes.CornflowerBlue, 100, 125)
+    End Sub
+
+    Private Sub form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PrintPreviewControl1.Document = PrintDocument1
+    End Sub
+End Class
