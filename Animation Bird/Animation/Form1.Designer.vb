443b9648b0cdb8ae8f280fc867ac75ca807878a4
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2021-03-23 13:52:59 +0000

    Add files via upload

---
+++ Animation Bird/Animation/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PicBird = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PicBird, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PicBird
+        '
+        Me.PicBird.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PicBird.Image = Global.Animation.My.Resources.Resources.bird1
+        Me.PicBird.Location = New System.Drawing.Point(108, 66)
+        Me.PicBird.Name = "PicBird"
+        Me.PicBird.Size = New System.Drawing.Size(177, 150)
+        Me.PicBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBird.TabIndex = 0
+        Me.PicBird.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Animation.My.Resources.Resources.Céu
+        Me.ClientSize = New System.Drawing.Size(418, 301)
+        Me.Controls.Add(Me.PicBird)
+        Me.Name = "Form1"
+        Me.Text = "BIRD"
+        CType(Me.PicBird, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PicBird As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Animation Bird/Animation/Form1.vb
@@ -0,0 +1,55 @@
+﻿'Link do Video -> https://www.youtube.com/watch?v=U9XuT9FDQYU
+
+Public Class Form1
+    Dim BirdCount As Integer = 1
+
+    'Timer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Select Case BirdCount
+            Case 1
+                PicBird.Image = My.Resources.bird1
+                BirdCount += 1
+            Case 2
+                PicBird.Image = My.Resources.bird2
+                BirdCount += 1
+            Case 3
+                PicBird.Image = My.Resources.bird3
+                BirdCount += 1
+            Case 4
+                PicBird.Image = My.Resources.bird4
+                BirdCount += 1
+            Case 5
+                PicBird.Image = My.Resources.bird5
+                BirdCount += 1
+            Case 6
+                PicBird.Image = My.Resources.bird6
+                BirdCount += 1
+            Case 7
+                PicBird.Image = My.Resources.bird7
+                BirdCount += 1
+            Case 8
+                PicBird.Image = My.Resources.bird8
+                BirdCount += 1
+            Case 9
+                PicBird.Image = My.Resources.bird9
+                BirdCount += 1
+            Case 10
+                PicBird.Image = My.Resources.bird10
+                BirdCount += 1
+            Case 11
+                PicBird.Image = My.Resources.bird11
+                BirdCount += 1
+            Case 12
+                PicBird.Image = My.Resources.birdLast
+                BirdCount = 1
+
+
+        End Select
+    End Sub
+
+    'Form Load
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+
+    End Sub
+End Class
