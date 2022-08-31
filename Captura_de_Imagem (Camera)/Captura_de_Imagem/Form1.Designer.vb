c27c0f4b049b2264d8e79b89b07433ee3670154e
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2022-08-31 10:32:49 +0100

    Add files via upload

---
+++ Captura_de_Imagem (Camera)/Captura_de_Imagem/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.iniciar_camera = New System.Windows.Forms.Button()
+        Me.capturar_imagem = New System.Windows.Forms.Button()
+        Me.salvar = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox2, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox1, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(14, 47)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(751, 335)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Yellow
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Location = New System.Drawing.Point(378, 3)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(369, 329)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Yellow
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(369, 329)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'iniciar_camera
+        '
+        Me.iniciar_camera.BackColor = System.Drawing.Color.Lime
+        Me.iniciar_camera.Font = New System.Drawing.Font("SimSun", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.iniciar_camera.Location = New System.Drawing.Point(17, 12)
+        Me.iniciar_camera.Name = "iniciar_camera"
+        Me.iniciar_camera.Size = New System.Drawing.Size(148, 29)
+        Me.iniciar_camera.TabIndex = 1
+        Me.iniciar_camera.Text = "Iniciar Camera"
+        Me.iniciar_camera.UseVisualStyleBackColor = False
+        '
+        'capturar_imagem
+        '
+        Me.capturar_imagem.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.capturar_imagem.Font = New System.Drawing.Font("SimSun", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.capturar_imagem.Location = New System.Drawing.Point(392, 12)
+        Me.capturar_imagem.Name = "capturar_imagem"
+        Me.capturar_imagem.Size = New System.Drawing.Size(164, 29)
+        Me.capturar_imagem.TabIndex = 2
+        Me.capturar_imagem.Text = "Capturar Imagem"
+        Me.capturar_imagem.UseVisualStyleBackColor = False
+        '
+        'salvar
+        '
+        Me.salvar.BackColor = System.Drawing.Color.Lime
+        Me.salvar.Font = New System.Drawing.Font("SimSun", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.salvar.Location = New System.Drawing.Point(660, 12)
+        Me.salvar.Name = "salvar"
+        Me.salvar.Size = New System.Drawing.Size(101, 29)
+        Me.salvar.TabIndex = 3
+        Me.salvar.Text = "Salvar Imagem"
+        Me.salvar.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(783, 406)
+        Me.Controls.Add(Me.salvar)
+        Me.Controls.Add(Me.capturar_imagem)
+        Me.Controls.Add(Me.iniciar_camera)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "STOP Camera"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents iniciar_camera As Button
+    Friend WithEvents capturar_imagem As Button
+    Friend WithEvents salvar As Button
+    Friend WithEvents stop_camera As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ Captura_de_Imagem (Camera)/Captura_de_Imagem/Form1.vb
@@ -0,0 +1,56 @@
+﻿'Link do Video -> https://www.youtube.com/watch?v=UerRxfj7qyM
+'Link do Código do Video -> https://laptrinhvb.net/index.php?/bai-viet/chuyen-de-vb-net/Huong-dan-chup-anh-tu-camera-su-dung-thu-vien-AForge-NET/1d5d0248cf4bfb26.html
+
+'NOTAS
+'Bitmap é Pixel
+'ShowDialog() - Mostra o formulário como uma caixa de diálogo modal.
+'Diálogo modal - É aquele que aparece na parte superior do conteúdo principal e move
+'o sistema para um modo especial que requer interação do usuário. Esta caixa de diálogo
+'desativa o conteúdo principal até
+'que o usuário interaja explicitamente com a caixa de diálogo modal 
+
+
+'Importações
+Imports AForge
+Imports AForge.Video
+Imports AForge.Video.DirectShow
+Imports System.IO
+
+Public Class Form1
+    'Variáveis
+    Dim Camera As VideoCaptureDevice
+    Dim bmp As Bitmap
+
+    'Liga a camera do Computador
+    Private Sub iniciar_camera_Click(sender As Object, e As EventArgs) Handles iniciar_camera.Click
+        Dim cameras As VideoCaptureDeviceForm = New VideoCaptureDeviceForm
+        If cameras.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Camera = cameras.VideoDevice
+            AddHandler Camera.NewFrame, New NewFrameEventHandler(AddressOf Capture)
+            Camera.Start()
+        End If
+    End Sub
+
+    'Faz com que o video funcione
+    Private Sub Capture(sender As Object, eventArgs As NewFrameEventArgs)
+        bmp = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+        PictureBox1.Image = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+    End Sub
+
+    'Tira a fotografia
+    Private Sub capturar_imagem_Click(sender As Object, e As EventArgs) Handles capturar_imagem.Click
+        PictureBox2.Image = PictureBox1.Image
+    End Sub
+
+    'Salvar a Imagem no computador
+    'A imagem nós é que decidimos onde queremos guardar no computador
+    Private Sub salvar_Click(sender As Object, e As EventArgs) Handles salvar.Click
+        'Formato pré definido por nós da imagem
+        SaveFileDialog1.DefaultExt = ".jpg"
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            'Abre a Janela para indicarmos onde queremos guardar a nossa fotografia
+            PictureBox2.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Jpeg)
+        End If
+    End Sub
+
+End Class
